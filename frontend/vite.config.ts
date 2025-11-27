cdcc79e1a2114dba50bbb637539eba41c0101591
Dinis Cunha <149959901+diniscunha20@users.noreply.github.com>
2025-11-27 13:16:25 +0000

    Revert "simpl register implementation + docker files"

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,3 @@
-import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -11,18 +10,4 @@ export default defineConfig({
       },
     }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-    },
-  },
 })
